b44f3fdc01c7f079e50f1760e867532750339b84
Mark Katerberg <katerberg@fastmail.fm>
2018-12-19 14:03:30 -0600

    Sample example

--- src/foo.js
+++ src/foo.js
@@ -1,4 +1,46 @@
 export default class Foo {
-  functionToTest() {
+  async functionToTest() {
+    try {
+      const data = await this.getData();
+      this.storedData = data;
+      return data.map(datum => ({name: datum}));
+    } catch(err) {
+      this.error = err;
+      this.storedData = [];
+      return this.storedData;
+    }
+
+//     return this.getData().then(data => {
+//       this.storedData = data;
+//       return data.map(datum => ({name: datum}));
+//     }).catch(err => {
+//       this.error = err;
+//       this.storedData = [];
+//       return this.storedData;
+//     });
+  }
+
+  multiSource() {
+    //get data from first and second and combine them
+    //after that get data from third
+    // return all combined
+  }
+
+  async getThirdData() {
+    return new Promise((resolve, reject) => {
+      resolve(['pierre']);
+    });
+  }
+
+  async getSecondData() {
+    return new Promise((resolve, reject) => {
+      resolve(['kelsey']);
+    });
+  }
+
+  async getData() {
+    return new Promise((resolve, reject) => {
+      resolve(['foo']);
+    });
   }
 }
--- src/foo.test.js
+++ src/foo.test.js
@@ -1,3 +1,4 @@
+import pDefer from 'p-defer';
 import Foo from './Foo';
 
 describe('Foo', () => {
@@ -6,5 +7,63 @@ describe('Foo', () => {
 
     expect(testObject).to.exist;
   });
-});
 
+  describe('getData', () => {
+    it('eventually gives back an array', (done) => {
+      const testObject = new Foo();
+
+      testObject.getData().then(response => {
+        expect(response).to.eql(['foo']);
+        done();
+      });
+    });
+  });
+
+  describe('functionToTest', () => {
+    it('gets data and turns it into an object', (done) => {
+      const testObject = new Foo();
+      const defer = pDefer();
+      sinon.stub(testObject, 'getData').returns(defer.promise);
+
+      testObject.functionToTest().then(response => {
+        expect(response).to.eql([{name: 'blah'}, {name: 'halb'}]);
+        done();
+      });
+
+      defer.resolve(['blah', 'halb']);
+    });
+
+    it('caches the data from getData when it comes back', (done) => {
+      const testObject = new Foo();
+      const defer = pDefer();
+      sinon.stub(testObject, 'getData').returns(defer.promise);
+
+      testObject.functionToTest();
+
+      expect(testObject.storedData).to.eql(undefined);
+      defer.resolve(['foo']);
+      defer.promise.then(response => {
+        expect(testObject.storedData).to.eql(['foo']);
+        done();
+      });
+    });
+
+    it('stores error message and hands back empty data when getData fails', (done) => {
+      const testObject = new Foo();
+      const defer = pDefer();
+      sinon.stub(testObject, 'getData').returns(defer.promise);
+
+      const responsePromise = testObject.functionToTest();
+
+      expect(testObject.error).to.eql(undefined);
+      defer.reject('idk what happened');
+
+      responsePromise.then((responseData) => {
+        expect(responseData).to.eql([]);
+        expect(testObject.storedData).to.eql([]);
+        expect(testObject.error).to.eql('idk what happened');
+        done();
+      });
+    });
+  });
+});
